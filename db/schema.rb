e9389bd505db8f4ffe74a44ae66c890b1fa55835
mario-j <mjordanbartolo@gmail.com>
2019-10-30 13:36:26 -0700

    HW8

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20191028183700) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.integer "age"
     t.decimal "height"
